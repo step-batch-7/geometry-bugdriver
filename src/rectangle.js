6f8a0aebe7f4b35dd121dfa0f5db61d023083c0b
= <=>
2019-12-17 14:12:47 +0530

    added isEqualTo method to Rectangle class

--- src/rectangle.js
+++ src/rectangle.js
@@ -8,6 +8,20 @@ class Rectangle {
   toString() {
     return `[Rectangle (${this.pointa.x},${this.pointa.y}) to (${this.pointc.x},${this.pointc.y})]`;
   }
+  isEqualTo(other) {
+    if (other === this) return true;
+    if (!(other instanceof Rectangle)) return false;
+    return (
+      this.pointa.isEqualTo(other.pointa) && this.pointc.isEqualTo(other.pointc)
+    );
+  }
+  get perimeter() {
+    const pointb = new Point(this.pointc.x, this.pointa.y);
+    return (
+      2 *
+      (this.pointa.findDistanceTo(pointb) + pointb.findDistanceTo(this.pointc))
+    );
+  }
 }
 
 module.exports = Rectangle;
--- test/testRectangle.js
+++ test/testRectangle.js
@@ -9,4 +9,37 @@ describe("Rectangle", () => {
       assert.strictEqual(rectangle.toString(), expected);
     });
   });
+
+  describe("#isEqualTo", () => {
+    it("should check if given object is same", () => {
+      const rectangle1 = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
+      const rectangle2 = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
+      assert.ok(rectangle1.isEqualTo(rectangle2));
+    });
+    it("should check if given object is not same", () => {
+      const rectangle1 = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
+      const rectangle2 = new Rectangle({ x: 1, y: 2 }, { x: 5, y: 4 });
+      assert.notOk(rectangle1.isEqualTo(rectangle2));
+    });
+    it("should check if given object is not instance of Rectangle", () => {
+      const rectangle1 = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
+      const r = { pointa: { x: 1, y: 1 }, pointc: { x: 5, y: 4 } };
+      assert.notOk(rectangle1.isEqualTo(r));
+    });
+    it("should check if same instance is passed", () => {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
+      assert.ok(rectangle.isEqualTo(rectangle));
+    });
+  });
+
+  describe("#perimeter", () => {
+    it("should give perimeter of rectangle if diagonal is not parallel to x of y axis", () => {
+      const rectangle = new Rectangle({ x: 8, y: 10 }, { x: 22, y: 5 });
+      assert.strictEqual(rectangle.perimeter, 38);
+    });
+    it("should give perimeter of rectangle if diagonal is parallel to x of y axis", () => {
+      const rectangle = new Rectangle({ x: 8, y: 5 }, { x: 22, y: 5 });
+      assert.strictEqual(rectangle.perimeter, 28);
+    });
+  });
 });
